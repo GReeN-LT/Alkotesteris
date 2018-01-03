e9c403c142ddf4cb3b775d7a9c6809df82c30afb
GReeN-LT <mindaugas.laboga23@gmail.com>
2018-01-03 23:23:50 +0200

    fontas

--- app/src/main/java/com/agobal/alkotesteris/MainActivity.java
+++ app/src/main/java/com/agobal/alkotesteris/MainActivity.java
@@ -1,5 +1,6 @@
 package com.agobal.alkotesteris;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,17 +17,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Typeface myTypeface = Typeface.createFromAsset(getAssets(), "A Sensible Armadillo.ttf");
+        TextView textView = (TextView) findViewById(R.id.textview);
+        textView.setTypeface(myTypeface);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
     @Override
