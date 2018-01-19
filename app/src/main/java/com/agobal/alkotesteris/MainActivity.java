4e17306a9e542617a57c5598ebb65b808f02116d
GReeN-LT <mindaugas.laboga23@gmail.com>
2018-01-20 00:19:05 +0200

    lytis

--- app/src/main/java/com/agobal/alkotesteris/MainActivity.java
+++ app/src/main/java/com/agobal/alkotesteris/MainActivity.java
@@ -6,28 +6,69 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public RadioGroup radioSexGroup;
+    public RadioButton radioSexButton;
+    //private Button btnDisplay;
+
+    RadioButton radioVyras;
+    RadioButton radioMoteris;
+
+
+
+    double vyras = 0.73;
+    double moteris = 0.66;
+
+    double r;
+
+
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-      //  Typeface myTypeface = Typeface.createFromAsset(getAssets(), "A Sensible Armadillo.ttf");
-      //  TextView textView = (TextView) findViewById(R.id.textview);
-      //  textView.setTypeface(myTypeface);
+        radioVyras = (RadioButton) findViewById(R.id.radioVyras);
+        radioMoteris = (RadioButton) findViewById(R.id.radioMoteris);
 
-       // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-       //
+        if(radioVyras.isChecked())
+        {
+            r = vyras;
+        }
+
+        else
+            r = moteris;
+
+        radioSexGroup = findViewById(R.id.RadioSex);
+        //btnDisplay = findViewById(R.id.prideti);
 
 
 
-    }
+                // get selected radio button from radioGroup
+                int selectedId = radioSexGroup.getCheckedRadioButtonId();
+
+
+                // find the radiobutton by returned id
+
+                radioSexButton = findViewById(selectedId);
+
+        Log.d("ADebugTag", "Value: " + r);
+
+        }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
