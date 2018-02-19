ad8410da6697951b3bc305aff9cca7df30afcc84
GReeN-LT <mindaugas.laboga23@gmail.com>
2018-02-19 04:48:05 +0200

    translate

--- app/src/main/java/com/agobal/alkotesteris/MainActivity.java
+++ app/src/main/java/com/agobal/alkotesteris/MainActivity.java
@@ -9,12 +9,14 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -22,7 +24,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO: vyr moter kofai blogai rodo
+    // TODO: vyr moter kofai blogai rodo, if edit text empty
 
     public RadioGroup radioSexGroup;
     public RadioButton radioSexButton;
@@ -32,7 +34,12 @@ public class MainActivity extends AppCompatActivity {
     RadioButton radioMoteris;
 
     Button skaiciuoti;
-    Button prideti;
+    ImageButton prideti;
+    ImageButton prideti2;
+    ImageButton delete;
+    ImageButton delete2;
+
+
 
 
     double vyras = 0.66;
@@ -40,15 +47,8 @@ public class MainActivity extends AppCompatActivity {
     double r;
     double BAC = 0;
 
-   // public static final String BAC1= "BAC";
-   // private static final int REQUEST_RESPONSE = 1;
-
 
     int valandos;
-
-
-
-
 
 
     @Override
@@ -62,9 +62,41 @@ public class MainActivity extends AppCompatActivity {
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
+        prideti2 = findViewById(R.id.plus2);
+
+        delete = findViewById(R.id.minus);
+        delete.setVisibility(View.GONE);
+        delete2 = findViewById(R.id.minus2);
+        delete2.setVisibility(View.GONE);
+        prideti2.setVisibility(View.GONE);
 
 
-        prideti = findViewById(R.id.prideti);
+
+        prideti = findViewById(R.id.plus);
+
+        skaiciuoti = findViewById(R.id.skaiciuoti);
+
+        final EditText svoris = findViewById(R.id.svoris);
+
+        final EditText mililitrai = findViewById(R.id.ml);
+
+        final EditText laipsniai = findViewById(R.id.laipsniai);
+
+
+        final EditText ml2 = findViewById(R.id.ml2);//
+
+
+        final EditText laipsniai2 = findViewById(R.id.laipsniai2);//
+
+        final EditText ml3 = findViewById(R.id.ml3);
+
+
+
+        final EditText laipsniai3 = findViewById(R.id.laipsniai3);
+
+
+
+
         prideti.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -75,15 +107,83 @@ public class MainActivity extends AppCompatActivity {
                 laipsniai2.setVisibility(View.VISIBLE);
 
                 prideti.setVisibility(View.GONE);
+                delete.setVisibility(View.VISIBLE);
+
+                prideti2.setVisibility(View.VISIBLE);
+            }
+        });
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ml2.setVisibility(View.GONE);
+                laipsniai2.setVisibility(View.GONE);
+
+                delete.setVisibility(View.GONE);
+
+                prideti2.setVisibility(View.GONE);
+                prideti.setVisibility(View.VISIBLE);
+
+                //trint reiksmes
+                ml2.setText("0");
+                laipsniai2.setText("0");
+
+            }
+        }); // delete butone?
+
+
+        prideti2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ml3.setVisibility(View.VISIBLE);
+                laipsniai3.setVisibility(View.VISIBLE);
+                delete.setVisibility(View.GONE);
+                delete2.setVisibility(View.VISIBLE);
+                prideti2.setVisibility(View.GONE);
+
+
+            }
+        });
+
+        delete2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                delete2.setVisibility(View.GONE);
+                prideti2.setVisibility(View.VISIBLE);
+                delete.setVisibility(View.VISIBLE);
+
+                ml3.setVisibility(View.GONE);
+                laipsniai3.setVisibility(View.GONE);
+
+                ml3.setText("0");
+                laipsniai3.setText("0");
             }
         });
 
 
-        skaiciuoti = findViewById(R.id.skaiciuoti);
+
         skaiciuoti.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view)
             {
+
+                if(TextUtils.isEmpty(svoris.getText()))
+                {
+                    Toast.makeText(getApplicationContext(), "Įveskite svorį!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(mililitrai.getText()))
+                {
+                    Toast.makeText(getApplicationContext(), "Įveskite mililitrus!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(laipsniai.getText()))
+                {
+                    Toast.makeText(getApplicationContext(), "Įveskite laipsnius!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
 
                 radioVyras = findViewById(R.id.radioVyras);
                 radioMoteris = findViewById(R.id.radioMoteris);
@@ -103,7 +203,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-                EditText svoris = findViewById(R.id.svoris);
+
                 String kg = svoris.getText().toString();
 
                 double KG = Double.parseDouble(kg);
@@ -111,13 +211,49 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("svoris" , String.valueOf(KG));
 
 
-                EditText mililitrai = findViewById(R.id.ml);
+
                 String ml = mililitrai.getText().toString();
 
 
-                EditText ml2 = findViewById(R.id.ml2);//
+
+                String laispniai = laipsniai.getText().toString();
+
+                if (ml.matches("") || laispniai.matches("")) {
+
+                    laispniai = ("0");
+                    ml = ("0");
+                }
+
+
+
+
                 String ml22 =  ml2.getText().toString();//
 
+
+                String laipsniai22 = laipsniai2.getText().toString();//
+
+                if (ml22.matches("") || laipsniai22.matches("")) {
+
+                    laipsniai22 = ("0");
+                    ml22 = ("0");
+                }
+
+
+
+                String ml33 = ml3.getText().toString();
+
+
+                String laipsniai33= laipsniai3.getText().toString();
+
+
+                if (ml33.matches("") || laipsniai33.matches("")) {
+
+                    laipsniai33 = ("0"); // keist
+                    ml33 = ("0"); // keist
+                }
+
+
+                double ML3 = Double.parseDouble(ml33);
                 double ML2 = Double.parseDouble(ml22);  // militrai 2
 
 
@@ -125,16 +261,16 @@ public class MainActivity extends AppCompatActivity {
 
                 Log.d("ML" , String.valueOf(ML));
 
-                EditText laipsniai = findViewById(R.id.laipsniai);
 
-                String laispniai = laipsniai.getText().toString();
 
-                EditText laipsniai2 = findViewById(R.id.laipsniai2);//
-                String laipsniai22 = laipsniai2.getText().toString();//
+
+
+
 
 
                 double PROM = Double.parseDouble(laispniai);
                 double PROM2 = Double.parseDouble(laipsniai22);//
+                double PROM3 = Double.parseDouble(laipsniai33);
 
                 Log.d("PROM" , String.valueOf(PROM));
 
@@ -142,12 +278,14 @@ public class MainActivity extends AppCompatActivity {
 
                 double StandGerimas = ML * PROM * 0.789 / 1000;
                 double StandGerimas2 = ML2 * PROM2 * 0.789 /1000;//
+                double StandGerimas3 = ML3 * PROM3 * 0.789 /1000;
 
                 Log.d("sG", String.valueOf(StandGerimas));
 
 
                 double alcoGramai = StandGerimas * 14;
                 double alcoGramai2 = StandGerimas2 * 14;//
+                double alcoGramai3 = StandGerimas3 * 14;
 
                 //konvertavimas
 
@@ -155,8 +293,7 @@ public class MainActivity extends AppCompatActivity {
 
                 double alcoGramOz = alcoGramai * 0.0338;
                 double alcoGramOz2 = alcoGramai2 * 0.0338;//
-
-
+                double alcoGramOz3 = alcoGramai3 * 0.0338;
 
 
                 BAC =  (alcoGramOz * 5.14 / Lbs * r) *10 ;
@@ -165,11 +302,15 @@ public class MainActivity extends AppCompatActivity {
                 double BAC2 = (alcoGramOz2 * 5.14 / Lbs * r) * 10 ;//
                 BAC2 = Math.round(BAC2*100.0) / 100.0;//
 
-                BAC = BAC2 + BAC;
+
+                double BAC3 = (alcoGramOz3 * 5.14 / Lbs * r) * 10 ;//
+                BAC3 = Math.round(BAC3*100.0) / 100.0;//
+
+
+                BAC = BAC + BAC2 + BAC3;
+
+
                 String BAC1 = String.valueOf(BAC);
-
-
-
         //        Log.d("bac", String.valueOf(BAC));
 
 
